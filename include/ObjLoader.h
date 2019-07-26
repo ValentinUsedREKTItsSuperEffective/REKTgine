b25182431c78f4e629fe29ada990baad7295140f
ValentinUsedREKTItsSuperEffective <valentin.bonnes@gmail.com>
2019-07-26 09:09:45 +0900

    Refactor ObjLoader

--- include/ObjLoader.h
+++ include/ObjLoader.h
@@ -7,14 +7,11 @@
 #include <vector>
 #include <string>
 
-class ObjLoader
-{
+class ObjLoader{
     public:
         ObjLoader();
         ~ObjLoader();
         bool load(std::string path, std::vector<glm::vec3> &vertices, std::vector<glm::vec2> &uvs, std::vector<glm::vec3> &normals);
-
-        private:
 };
 
 #endif // OBJLOADER_H
--- src/ObjLoader.cpp
+++ src/ObjLoader.cpp
@@ -4,22 +4,14 @@
 #include <stdio.h>
 #include <cstring>
 
-ObjLoader::ObjLoader()
-{
-    //ctor
-}
+ObjLoader::ObjLoader(){}
 
-ObjLoader::~ObjLoader()
-{
-    //dtor
-}
+ObjLoader::~ObjLoader(){}
 
 bool ObjLoader::load(std::string path, std::vector<glm::vec3> &verticesOut, std::vector<glm::vec2> &uvsOut, std::vector<glm::vec3> &normalsOut){
-
     std::vector<unsigned int> verticesIndex, uvsIndex, normalsIndex;
-    std::vector<glm::vec3> tmp_vertices;
+    std::vector<glm::vec3> tmp_vertices, tmp_normals;
     std::vector<glm::vec2> tmp_uvs;
-    std::vector<glm::vec3> tmp_normals;
 
     FILE * file = fopen(path.c_str(), "r");
     if(!file){
@@ -27,38 +19,31 @@ bool ObjLoader::load(std::string path, std::vector<glm::vec3> &verticesOut, std:
         return false;
     }
 
-    while(!feof(file))
-    {
+    while(!feof(file)){
         char header[256];
         fscanf(file, "%s", header);
 
-        // vertices
-        if(strcmp("v",header)==0){
+        if(strcmp("v",header) == 0){ // vertices
             glm::vec3 vertex;
             fscanf(file,"%f" "%f" "%f\n", &vertex.x, &vertex.y, &vertex.z);
             tmp_vertices.push_back(vertex);
-        }
-        // uv
-        else if(strcmp("vt",header)==0){
+        } else if(strcmp("vt",header) == 0){ // uv
             glm::vec2 uv;
             fscanf(file, "%f %f\n", &uv.x, &uv.y);
             tmp_uvs.push_back(uv);
-        }
-        // normal
-        else if(strcmp("vn",header)==0){
+        } else if(strcmp("vn",header) == 0){ // normal
             glm::vec3 normal;
             fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
             tmp_normals.push_back(normal);
-        }
-        // face (cas particulier de triangle à trois composante par sommets)
-        else if(strcmp("f",header)==0){
+        } else if(strcmp("f",header) == 0){ // face
             int vertexIndex[3], uvIndex[3], normalIndex[3];
             int matches = fscanf(file,"%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
 
-            if(matches =! 9){
+            if(matches != 9){
                 std::cout << "Wrong data format !" << std::endl;
                 return false;
             }
+
             verticesIndex.push_back(vertexIndex[0]);
             verticesIndex.push_back(vertexIndex[1]);
             verticesIndex.push_back(vertexIndex[2]);
